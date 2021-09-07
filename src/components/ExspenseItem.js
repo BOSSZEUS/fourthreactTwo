5be60b638894fd4a758bc315876acba5c5219cd9
BOSSZEUS <jordanscheftic@yahoo.com>
2021-09-07 01:08:42 -0700

    styling components with css

--- src/components/ExspenseItem.js
+++ src/components/ExspenseItem.js
@@ -1,5 +1,14 @@
-function ExspenseItem () {
-    return <h2>Exspense Item!!!</h2>
+import './ExspenseItem.css'
+function ExspenseItem() {
+    return (
+        <div className='expense-item'>
+            <div>March, 28th 2022</div>
+            <div className='expense-item__description'>
+                <h2>Car insurance</h2>
+                <div className='expense-item__price'>500</div>
+            </div>
+        </div>)
+
 }
 
 export default ExspenseItem
