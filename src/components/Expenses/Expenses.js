386054ad88b57c54d2d69ed554027f00e01d4fa2
Jordan Scheftic <47509698+BOSSZEUS@users.noreply.github.com>
2021-10-02 14:52:42 -0700

    expensefilter and expense selected year now work

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,34 +1,23 @@
 import React, { useState } from 'react'
 import './Expenses.css'
-import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
 import Expensefilter from './ExpenseFilter'
+import ExpensesList from './ExpensesList'
 
 const Expenses = (props) => {
     const [FilteredYear, setFilteredYear] = useState('2020')
-    const filterchangeHandler = selectedYear =>{
-        setFilteredYear()
+    const filterchangeHandler = (selectedYear) =>{
+        setFilteredYear(selectedYear)
     }
+    const filteredExpenses = props.items.filter((expense)=>{
+        return expense.date.getFullYear().toString() === FilteredYear
+    })
  
     return(
         <div>
         <Card className='expenses'>
         <Expensefilter selected={FilteredYear} onChangeFilter={filterchangeHandler} />
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}>
-            </ExpenseItem>
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}>
-            </ExpenseItem>
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}>
-            </ExpenseItem>
+        <ExpensesList items={filteredExpenses} />
         </Card>
         </div>
     )
